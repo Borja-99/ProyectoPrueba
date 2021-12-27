2a4f5dfe5560633c242aea8ac8d3d4cf03fb3f90
Borja <borja.estevez-penalver@capgemini.com>
2021-12-27 10:50:23 +0100

    minor changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbar, MatToolbarModule } from '@angular/material/toolbar';
+import { MatToolbarHarness } from '@angular/material/toolbar/testing';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +15,11 @@ import { HeaderComponent } from './components/header/header.component';
     HeaderComponent
   ],
   imports: [
+    RouterModule,
+    MatButtonModule,
+    MatToolbar,
+    MatToolbarHarness,
+    MatToolbarModule,
     BrowserModule,
     AppRoutingModule
   ],
